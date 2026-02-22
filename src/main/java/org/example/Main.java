407f174710b39d4205936b8adb20934b205b04c5
Jonas Nascimento <jonasnascimemto091@gmail.com>
2026-02-22 19:11:08 -0300

    maior e posicao

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class maior-posicao{
+public class maior{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
