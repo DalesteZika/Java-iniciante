8c47fe4432e1f5cbe6eb8e2006d6cb97b235760d
Jonas Nascimento <jonasnascimemto091@gmail.com>
2026-02-22 19:12:27 -0300

    teste

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class maior{
+public class teste{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
